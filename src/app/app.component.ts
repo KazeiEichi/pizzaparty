6fecc5dc2f060c36804d4a8148164c3daeda9bbc
Weber-Marquiset Nolwenn <nolwenn.weber-marquiset@laposte.net>
2019-03-25 12:50:48 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,19 @@
 import { Component } from '@angular/core';
 
+export class Pizza {
+  id : number;
+  name: string;
+  price : number;
+  photo : string;
+}
+const PIZZAS : Pizza[] = [
+  { id: 1, name: 'Reine', price: 12.99, photo: 'pizza-1.jpg' },
+  { id: 2, name: '4 fromages', price: 13.99, photo: 'pizza-2.jpg' },
+  { id: 3, name: 'Orientale', price: 11.99, photo: 'pizza-3.jpg' },
+  { id: 4, name: 'Cannibale', price: 9.99, photo: 'pizza-4.jpg' }
+];
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +21,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pizzaparty';
+  pizza: Pizza = {
+    id: 1,
+    name: 'Reine',
+    price : 12.99,
+    photo : ''
+  };
+  PizzaList = PIZZAS;
 }
